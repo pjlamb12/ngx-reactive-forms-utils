23fe4002553ad5186c0613e15a5a9cd1bca7a9bf
Preston Lamb <preston.j.lamb@gmail.com>
2022-12-01 00:25:22 -0700

    finished up a demo form to show the functionality of the library

--- apps/demo-app/src/app/demo-form/demo-form.component.ts
+++ apps/demo-app/src/app/demo-form/demo-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { addCustomErrorMessage, CustomValidators } from 'ngx-reactive-forms-utils';
 
 @Component({
 	selector: 'ngx-reactive-form-utils-demo-form',
@@ -6,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./demo-form.component.scss'],
 })
 export class DemoFormComponent implements OnInit {
-	constructor() {}
+	private options: AbstractControlOptions = {
+		validators: [CustomValidators.confirmStringMatch('password', 'confirmPassword')],
+	};
+	public form: FormGroup = this._fb.nonNullable.group(
+		{
+			numberOnly: [null, [Validators.required, CustomValidators.number]],
+			phone: ['', [Validators.required, CustomValidators.phoneNumber]],
+			zipCode: ['', [Validators.required, CustomValidators.validZipCode]],
+			password: ['', [Validators.required]],
+			confirmPassword: ['', [Validators.required]],
+		},
+		this.options,
+	);
 
-	ngOnInit(): void {}
+	constructor(private _fb: FormBuilder) {}
+
+	ngOnInit(): void {
+		addCustomErrorMessage('phoneNumber', () => 'Please enter a valid phone number');
+		addCustomErrorMessage('validZipCode', () => 'Please enter a valid zip code');
+	}
 }
