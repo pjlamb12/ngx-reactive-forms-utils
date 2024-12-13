d79617fc921bc36999f00fa2c54befd63d21106e
Preston Lamb <preston.j.lamb@gmail.com>
2024-12-12 22:04:16 -0700

    chore: remove unused import of default component that was deleted

--- apps/demo-app/src/app/app.module.ts
+++ apps/demo-app/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsUtilsModule } from 'ngx-reactive-forms-utils';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { DemoFormComponent } from './demo-form/demo-form.component';
 
 @NgModule({
-	declarations: [AppComponent, NxWelcomeComponent, DemoFormComponent],
+	declarations: [AppComponent, DemoFormComponent],
 	imports: [BrowserModule, ReactiveFormsModule, ReactiveFormsUtilsModule],
 	providers: [],
 	bootstrap: [AppComponent],
