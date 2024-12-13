b2be0728baf5a46f80eb1d5975c0f0621716eead
Preston Lamb <preston.j.lamb@gmail.com>
2024-12-12 21:49:52 -0700

    chore: remove the old module
    relates to #13

--- libs/reactive-forms-utils/src/index.ts
+++ libs/reactive-forms-utils/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/reactive-forms-utils.module';
 export * from './lib/control-errors-display/control-errors-display.component';
 export * from './lib/custom-error-message-utils';
 export * from './lib/custom-validators';
